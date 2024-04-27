3d126b65a51d4d00397b5df7d53fe076191ec6d9
Jeremy Silver <jeremys@nessiness.com>
2024-04-26 20:33:30 -0400

    refactor: better output format for py_loc_summarize

--- scripts/py_loc_summarize.sh
+++ scripts/py_loc_summarize.sh
@@ -2,4 +2,10 @@
 
 echo "LINE STATS"
 echo "----------"
-radon raw $1 -s | tail -n 11 | head -n 7 | awk '{$1=$1};1'
+radon raw $1 -s | tail -n 11 | head -n 7 | python3 -c '
+import sys
+pairs = [line.strip().split(": ", maxsplit=1) for line in sys.stdin]
+pairs = [((key + ":").ljust(16), val) for (key, val) in pairs]
+width = len(pairs[0][1])
+for (key, val) in pairs:
+    print(f"{key} {val.rjust(width)}")'

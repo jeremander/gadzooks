7370578a9f1ff5748bd0ab13049746ddefb928dc
Jeremy Silver <jeremys@nessiness.com>
2024-04-28 10:54:20 -0400

    feat: print version with --version

--- gadzooks/main.py
+++ gadzooks/main.py
@@ -3,7 +3,7 @@
 import argparse
 from typing import cast
 
-from gadzooks import Subcommand
+from gadzooks import Subcommand, __version__
 from gadzooks.check_version import CheckVersion
 from gadzooks.loc_summarize import LinesOfCodeSummarize
 
@@ -16,6 +16,7 @@ SUBCOMMANDS: dict[str, type[Subcommand]] = {
 def main() -> None:
     """Main entry point for gadzooks executable."""
     parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='store_true', help='print version number')
     subparsers = parser.add_subparsers(dest='subcommand')
     for (name, cls) in SUBCOMMANDS.items():
         doc = cast(str, cls.__doc__)
@@ -23,6 +24,11 @@ def main() -> None:
         subparser = subparsers.add_parser(name, description=descr, help=doc)
         cls.configure_parser(subparser)
     args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        return
+    if not args.subcommand:
+        parser.error('the following arguments are required: subcommand')
     SUBCOMMANDS[args.subcommand].main(args)
 
 
